7e4c9c3ce1e28f5f7e418dab9cc5450d78bb1b18
Joshua <jdupin@topics.fr>
2023-02-13 20:50:54 +0100

    conflicts merge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
@@ -14,8 +15,26 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+>>>>>>> 4a9d584 (Initialize project using Create React App)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+<<<<<<< HEAD
 reportWebVitals()
+=======
+reportWebVitals();
+>>>>>>> 4a9d584 (Initialize project using Create React App)
